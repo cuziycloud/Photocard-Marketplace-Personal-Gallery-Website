5aa8902defb41f5f1febe73e4749590539932600
mvzan <165400589+cuziycloud@users.noreply.github.com>
2025-05-15 01:26:49 +0700

    fix structure

--- Clz/Clz/src/main/java/com/kpop/Clz/repository/ProductRepository.java
+++ Clz/Clz/src/main/java/com/kpop/Clz/repository/ProductRepository.java
@@ -1,4 +1,10 @@
-package com.kpop.Clz.Repository;
+package com.kpop.Clz.repository;
 
-public class ProductRepository {
+import com.kpop.Clz.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
